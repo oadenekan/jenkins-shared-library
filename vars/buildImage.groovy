ad563f3f070b8b2444aeaddebc8eb19f138aff3f
Olusola Ayeni <adenekan.jumoke@gmail.com>
2025-06-20 17:39:42 +0100

    Corrected the credential error found

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call () {
     echo "building the docker image..."
-    withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t olusolaayeni/demo-app:3.0 ."
-        sh "echo '${PASS}' | docker login -u '${USER}' --password-stdin"
-        sh "docker push olusolaayeni/demo-app:3.0"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t olusolaayeni/demo-app:2.0 ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push olusolaayeni/demo-app:2.0"
     }
 }
