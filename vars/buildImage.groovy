1933e5258f3d3e83e3fb3f79ceb06f366b187dd4
Olusola Ayeni <adenekan.jumoke@gmail.com>
2025-06-22 01:31:03 +0100

    Remove extra space while defining function

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
-def call () {
+def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t olusolaayeni/demo-app:2.0 ."
